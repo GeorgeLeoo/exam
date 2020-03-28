aaaf989a1bb2b18f91aea78d1a502f872b111f2e
GeorgeLeeo <georgeleeo@163.com>
2020-03-29 00:08:20 +0800

    Update index.vue

--- src/views/Exam/index.vue
+++ src/views/Exam/index.vue
@@ -2,7 +2,7 @@
   <section class="exam">
     <p class="tip">在这里您可以进行正式考试，输入对应场次的考试密码即可进行考试。</p>
     <el-input v-model="password" placeholder="请输入考试密码" class="gap"/>
-    <el-button type="primary" round class="gap check">验证密码</el-button>
+    <el-button type="primary" round class="gap check" @click="handleVerify">验证密码</el-button>
   </section>
 </template>
 
@@ -18,6 +18,11 @@ export default {
   data () {
     return {
       password: ''
+    }
+  },
+  methods: {
+    handleVerify () {
+      this.$router.push('/examing')
     }
   }
 }
